0da290e7d807d69df6ab596fc42c24704cf5e66b
Jiyoon Kim <jiyoon9886@gmail.com>
2020-05-19 17:14:10 -0400

    created route for homepage

--- server.js
+++ server.js
@@ -15,65 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
 });
 
-db.User.create({ name: "Ernest Hemingway" })
-  .then((dbUser) => {
-    console.log(dbUser);
-  })
-  .catch(({ message }) => {
-    console.log(message);
-  });
-
-app.get("/notes", (req, res) => {
-  db.Note.find({})
-    .then((dbNote) => {
-      res.json(dbNote);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-app.get("/user", (req, res) => {
-  db.User.find({})
-    .then((dbUser) => {
-      res.json(dbUser);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-app.post("/submit", ({ body }, res) => {
-  db.Note.create(body)
-    .then(({ _id }) =>
-      db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true })
-    )
-    .then((dbUser) => {
-      res.json(dbUser);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-app.get("/populateduser", (req, res) => {
-  // TODO
-  // =====
-  // Write the query to grab the documents from the User collection,
-  // and populate them with any associated Notes.
-  // TIP: Check the models out to see how the Notes refers to the User
-  db.User.find({})
-    .populate("notes")
-    .then((userNotes) => {
-      res.json(userNotes);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+app.get("/", (req, res) => {
+  res.send(index.html);
 });
 
 // Start the server
